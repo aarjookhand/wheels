568c143895fde55e6b03e853bfc282412c91f7ee
aarjookhand <03aarjoo@gmail.com>
2023-12-23 17:24:06 +0100

    baisc login and registration
    working

---
+++ auth.py
@@ -0,0 +1,368 @@
+from flask import *
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from flask_login import LoginManager, UserMixin, login_user, current_user 
+from flask_babel import Babel, _
+import sqlite3
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from gridfs import GridFS
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+
+
+
+auth_blueprint = Blueprint('auth_blueprint', __name__)
+babel = Babel()
+
+
+
+LANGUAGES = ['en', 'fr', 'es']
+
+@babel.localeselector
+def get_locale():
+    user_language = request.args.get('lang', 'en')
+
+    if user_language not in LANGUAGES:
+        user_language = 'en'
+    return user_language
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth_blueprint.login'
+login_manager.init_app(auth_blueprint)
+
+
+load_dotenv()
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_DB = os.environ.get("MONGODB_DB")
+MONGODB_COLLECTION = os.environ.get("MONGODB_COLLECTION")
+SQLITE_URI = os.environ.get('SQLITE_URI', 'uber.db')
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'False').lower() == 'true'
+MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'False').lower() == 'true'
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+
+
+
+# conn to MongoDB
+client = MongoClient(MONGODB_URI)
+db = client[MONGODB_DB]
+rides_collection = db['rides']
+fs = GridFS(db)
+
+
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+conn = sqlite3.connect(SQLITE_URI)
+cursor = conn.cursor()
+
+
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  fullname TEXT NOT NULL, 
+                  email TEXT NOT NULL,
+                  username TEXT NOT NULL,
+                  password TEXT NOT NULL
+                  )''')
+
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS drivers
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  fullname TEXT NOT NULL, 
+                  email TEXT NOT NULL,
+                  username TEXT NOT NULL,
+                  password TEXT NOT NULL,
+                  
+                  vehicle TEXT NOT NULL)''')
+
+conn.commit()
+conn.close()
+
+
+class User(UserMixin):
+    def __init__(self, id,  fullname, email, username, password):
+         self.id = id
+         self.fullname = fullname
+         self.email = email
+         self.username = username
+         self.password = password  
+
+    def get_id(self):
+         return str(self.email)
+    
+
+
+class Driver(UserMixin):
+    def __init__(self, id, fullname, email, username, password, vehicle):
+        self.id = id
+        self.fullname = fullname
+        self.email = email
+        self.username = username
+        self.password = password
+        self.vehicle = vehicle
+
+    def get_id(self):
+         return str(self.email)
+
+  
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect(SQLITE_URI)
+    cursor = conn.cursor()
+
+   
+    cursor.execute("SELECT * FROM users WHERE email = ?", (user_id,))
+    user_data = cursor.fetchone()
+
+    if user_data:
+        user = User(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4])
+        conn.close()
+        return user
+
+ 
+    cursor.execute("SELECT * FROM drivers WHERE email = ?", (user_id,))
+    driver_data = cursor.fetchone()
+    conn.close()
+
+    if driver_data:
+        driver = Driver(driver_data[0], driver_data[1], driver_data[2], driver_data[3], driver_data[4], driver_data[5])
+        print('Logged in as driver')
+        return driver
+
+    print('Invalid user')
+    return None
+
+
+
+  
+
+
+
+
+
+
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
+
+
+
+class DriverRegistrationForm(FlaskForm):
+    fullname = StringField('Full Name', validators=[DataRequired(), Length(min=4, max=100)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])    
+    vehicle_class_choices = [('car', 'Car'), ('motorcycle', 'Motorcycle')]
+    vehicle_class = SelectField('Vehicle Class', choices=vehicle_class_choices, validators=[DataRequired()])
+    submit = SubmitField('Sign Up')    
+
+    def validate_username(self, field):
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM drivers WHERE username = ?", (field.data,))
+        existing_user = cursor.fetchone()
+        conn.close()
+
+        if existing_user:
+            raise ValidationError('username alr taken')
+
+
+    def validate_email(self, field):
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ?", (field.data,))
+        existing_user = cursor.fetchone()
+        conn.close()
+
+        if existing_user:
+            raise ValidationError('email alr taken')
+        
+
+
+class UserRegistrationForm(FlaskForm):
+    fullname = StringField('Full Name', validators=[DataRequired(), Length(min=4, max=100)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])    
+
+    submit = SubmitField('Sign Up')    
+
+    def validate_username(self, field):
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (field.data,))
+        existing_user = cursor.fetchone()
+        conn.close()
+
+        if existing_user:
+            raise ValidationError('username alr taken')
+
+
+    def validate_email(self, field):
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ?", (field.data,))
+        existing_user = cursor.fetchone()
+        conn.close()
+
+        if existing_user:
+            raise ValidationError('email alr taken')
+        
+
+    
+
+### routes
+
+@auth_blueprint.route('/', methods=['GET'])
+def display_welcome():
+    return render_template('welcome.html', get_locale=get_locale)
+
+
+
+@auth_blueprint.route('/register_user', methods=['GET', 'POST'])
+def register_user():
+    form = UserRegistrationForm()
+
+
+    if form.validate_on_submit():
+
+        fullname = form.fullname.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+
+
+   
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        existing_user = cursor.fetchone()
+        
+        if existing_user:
+            conn.close()
+            flash("Username already exists. Please choose a different one.")
+     
+        else:
+            cursor.execute("INSERT INTO users ( fullname, email, username, password) VALUES (?, ?, ?, ?)", (fullname, email, username,  password))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('auth_blueprint.login'))
+
+    return render_template('user_signup.html', form=form, get_locale=get_locale)
+
+
+
+@auth_blueprint.route('/register_driver', methods=['GET', 'POST'])
+def register_driver():
+    form = DriverRegistrationForm()
+
+    if form.validate_on_submit():
+        fullname = form.fullname.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        vehicle = form.vehicle_class.data
+   
+        conn = sqlite3.connect(SQLITE_URI)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM drivers WHERE username = ?", (username,))
+        existing_user = cursor.fetchone()
+        
+        if existing_user:
+            conn.close()
+            flash("Username already exists. Please choose a different one.")
+        else:
+            cursor.execute("INSERT INTO drivers ( fullname, email, username, password, vehicle) VALUES (?, ?, ?, ?, ?)", (fullname, email, username,  password, vehicle))
+            conn.commit()
+            conn.close()
+
+            user_doc = {
+                'fullname': fullname,
+                            
+                'email': email,
+                'username': username,
+                'vehicle': vehicle 
+                
+            }
+            db.drivers.insert_one(user_doc)
+
+
+
+            flash("Successfully registered. You can now log in.")
+            return redirect(url_for('auth_blueprint.login'))
+
+    return render_template('driver_signup.html', form=form, get_locale=get_locale)
+
+
+
+
+
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.is_submitted():
+        username = form.username.data
+        password = form.password.data
+
+        conn_users = sqlite3.connect(SQLITE_URI)
+        cursor_users = conn_users.cursor()
+        cursor_users.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user_data = cursor_users.fetchone()
+        conn_users.close()
+
+        if not user_data:
+            conn_drivers = sqlite3.connect(SQLITE_URI)
+            cursor_drivers = conn_drivers.cursor()
+            cursor_drivers.execute("SELECT * FROM drivers WHERE username = ? AND password = ?", (username, password))
+            driver_data = cursor_drivers.fetchone()
+            conn_drivers.close()
+
+            if driver_data:
+                driver = Driver(driver_data[0], driver_data[1], driver_data[2], driver_data[3], driver_data[4], driver_data[5])
+                login_user(driver)
+                print('logged in as driver')
+                return redirect(url_for('driver_blueprint.index'))
+
+        if user_data:
+            user = User(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4])
+            login_user(user)
+            print('logged in as user')
+            return redirect(url_for('user_blueprint.index'))
+       
+    return render_template('login.html', form=form, get_locale=get_locale)
+
+
+
+
+
+
+@auth_blueprint.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('auth_blueprint.display_welcome'))
+
+
+
+
---
+++ driver.py
@@ -0,0 +1,250 @@
+from flask import *
+from pymongo import MongoClient
+from flask_login import login_required, current_user
+from dotenv import load_dotenv
+from flask_babel import Babel, _
+import base64
+from flask_mail import Message, Mail
+from elasticsearch import Elasticsearch
+import folium
+from folium.plugins import MiniMap
+import sqlite3
+from bson import ObjectId
+from bson.errors import InvalidId
+from auth import Driver, User
+from flask_mail import Message, Mail
+
+
+
+
+load_dotenv()
+import os
+
+
+driver_blueprint = Blueprint('driver_blueprint', __name__)
+babel = Babel()
+mail = Mail()
+ 
+
+LANGUAGES = ['en', 'fr', 'es']
+
+@babel.localeselector
+def get_locale():
+    user_language = request.args.get('lang', 'en')
+
+    if user_language not in LANGUAGES:
+        user_language = 'en'
+    return user_language
+
+
+
+driver_blueprint.secret_key = os.environ.get("SECRET_KEY", "default_secret_key")
+
+
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_DB = os.environ.get("MONGODB_DB")
+MONGODB_COLLECTION = os.environ.get("MONGODB_COLLECTION")
+MONGODB_COLLECTION_DRIVERS = os.environ.get("MONGODB_COLLECTION_DRIVERS")
+SQLITE_URI = os.environ.get('SQLITE_URI', 'uber.db')
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'False').lower() == 'true'
+MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'False').lower() == 'true'
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+
+
+
+# Connect to MongoDB
+client = MongoClient(MONGODB_URI)
+db = client[MONGODB_DB]
+collection = db[MONGODB_COLLECTION]
+
+
+@driver_blueprint.route('/home_driver', methods=['GET', 'POST'])
+@login_required
+def index():
+    map_obj = initialize_map()
+
+
+    if isinstance(current_user, Driver):
+        vehicle = current_user.vehicle
+    else:
+        
+        vehicle = None
+
+    ride_requests_collection = db['ride_requests']
+    open_orders = ride_requests_collection.find({'status': 'open', 'vehicle_type': vehicle})
+
+    drivers = db.drivers.find({'vehicle_type': vehicle})
+
+    return render_template('driver_homepage.html', map_content=map_obj._repr_html_(), open_orders=open_orders, drivers=drivers)
+
+def initialize_map():
+    
+    map_obj = folium.Map(location=[48.8566, 2.3522], zoom_start=15)
+    folium.Marker([48.8566, 2.3522]).add_to(map_obj)
+    MiniMap().add_to(map_obj)
+    return map_obj
+
+
+
+
+
+@driver_blueprint.route('/details/<order_id>', methods=['GET', 'POST'])
+def details(order_id):
+
+    order = db.ride_requests.find_one({'_id': ObjectId(order_id)})
+
+    if not order:
+        return "Order not found."
+
+    if request.method == 'POST':
+        driver_id = current_user.username
+        db.ride_requests.update_one(
+            {'_id': ObjectId(order_id)},
+            {'$set': {'status': 'received',  'driver_username': driver_id}},
+           
+        )
+
+        flash("Order received successfully!")
+        return redirect(url_for('driver_blueprint.details', order_id=order_id))
+
+    return render_template('ride_details.html', order=order)
+
+
+
+@driver_blueprint.route('/invoice/<order_id>')
+def display_invoice(order_id):
+    ride_requests_collection = db['ride_requests']
+    order = ride_requests_collection.find_one({'_id': ObjectId(order_id)})
+
+    if not order:
+        return "Order not found."
+    
+    ride_requests_collection.update_one(
+        {'_id': ObjectId(order_id)},
+        {'$set': {'status': 'completed'}}
+    )
+
+    conn = sqlite3.connect(SQLITE_URI)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email = ?", (order['user_email'],))
+    user_data = cursor.fetchone()
+    conn.close()
+
+    if not user_data:
+        return "User not found."
+
+    user = User(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4])
+    driver_collection = db['drivers']
+    driver_username = order.get('driver_username')
+
+    if driver_username:
+        driver = driver_collection.find_one({'username': driver_username})
+        if not driver:
+            return "Driver not found."
+    else:
+        return "Driver username not found."
+
+    return render_template('driver_invoice.html', order=order, user=user, driver=driver)
+
+
+def send_invoice_email(recipient, order, user, driver):
+    subject = 'Invoice for Your Ride'
+    body = f'''
+    Dear {user.username},
+
+    Thank you for using our service! Below is the invoice for your recent ride:
+
+    Order ID: {order['_id']}
+    User ID: {user.id}
+    User Email: {user.email}
+    Driver Username: {driver.username}
+    Total Cost: {order['total_cost']} €
+
+    We appreciate your business.
+
+    Best regards,
+    Your Company Name
+    '''
+
+    sender = 'your_company_email@example.com'  # Update with your company email
+    message = Message(subject, recipients=[recipient], body=body, sender=sender)
+    mail.send(message)
+
+
+
+
+@driver_blueprint.route('/driver_profile', methods=['GET', 'POST'])
+def view_profile():
+    user_object = db.drivers.find_one({'username': current_user.username})
+
+    if not user_object:
+     
+        flash("User not found.")
+        return redirect(url_for('driver_blueprint.index'))
+
+    driver_info = {
+        'username': user_object['username'],
+        'email': user_object['email'],
+        'profile_picture': user_object.get('profile_picture', None)  
+    }
+
+
+    if request.method == 'POST':
+        new_email = request.form.get('new_email')
+        profile_picture = request.files.get('profile_picture')
+
+        try:
+            if new_email:
+                result_email = db.drivers.update_one(
+                    {'username': current_user.username},
+                    {'$set': {'email': new_email}}
+                )
+                print(result_email.raw_result)
+                flash("Email updated successfully!")
+
+            if profile_picture and not driver_info['profile_picture']:
+                
+                encoded_picture = base64.b64encode(profile_picture.read()).decode('utf-8')
+                result_picture = db.drivers.update_one(
+                    {'username': current_user.username},
+                    {'$set': {'profile_picture': encoded_picture}}
+                )
+                print(result_picture.raw_result)
+                flash("Profile picture added successfully!")
+
+        except InvalidId:
+            flash(f"Invalid ObjectId: {current_user.username} is not a valid ObjectId")
+
+        return redirect(url_for('driver_blueprint.view_profile'))
+
+
+    driver_document = db.drivers.find_one({'username': current_user.username})
+    if driver_document and 'profile_picture' in driver_document:
+        driver_info['profile_picture'] = driver_document['profile_picture']
+
+    return render_template('driver_profile.html', driver_info=driver_info)
+
+
+
+
+@driver_blueprint.route('/delete_profile_picture', methods=['POST'])
+def delete_profile_picture():
+    try:
+        result = db.drivers.update_one(
+            {'username': current_user.username},
+            {'$unset': {'profile_picture': 1}}
+        )
+        print(result.raw_result)
+        flash("Profile picture deleted successfully!")
+    except InvalidId:
+        print(f"Invalid ObjectId: {current_user.username} is not a valid ObjectId")
+
+    return redirect(url_for('driver_blueprint.view_profile'))
+
+
+
